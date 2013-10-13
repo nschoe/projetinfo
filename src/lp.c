178dfddc7ca0711554002ede439e0b49137d3702
nschoe <ns.schoe@gmail.com>
2013-10-13 02:05:38 +0200

    First function lp working : checkign for parameters to be an existing, readable file and fires a warning when too much arguments are given

--- src/lp.c
+++ src/lp.c
@@ -2,15 +2,53 @@
 #include <stdio.h>
 #include <string.h>
 
+#include "notify.h"
+
 #include "lp.h"
 
+/* Check that one and exactly one file argument has been given
+   and that it exists and is readable
+*/
 int parseLp( char * args )
 {
-    char * pch;
-    char * delim = " ";
-    
-    printf( "args : \"%s\"\n", args );
+    char * argsCopy = malloc( strlen( args ) * sizeof( char ) );
+    char * pch = NULL;
+    char delim[] = " ";
+    FILE * h = NULL;
 
+    // Copy the string argument to use strtok on it
+    strcpy( argsCopy, args );
+
+    // First cut
+    pch = strtok (argsCopy, delim);
+    
+    // Check if we have one argument given
+    if( NULL == pch )
+    {
+	WARNING_MSG( "A filename must be given to the function 'lp'\n" );
+    }
+    else
+    {
+	// Check if the file exists and is readable
+	h = fopen( pch, "r" );
+	if( NULL == h )
+	{
+	    WARNING_MSG( "The file \"%s\" doesn't exists or you don't have the permissions to read it.\n", pch );
+	}
+	else
+	    fclose( h );
+
+	// Do a second cut to check that only one file was given
+	pch = strtok (NULL, delim);
+
+	if( NULL != pch )
+	{
+	    WARNING_MSG( "The function lp only takes the first argument into account and will ignore the others.\n" );
+	}
+
+    }
+
+    
     return 0;
 }
 
