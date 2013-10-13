105e53962f85680593c8fd8afb63ba76c761aa9a
FELTRIN <feltrin.eric@gmail.com>
2013-10-13 16:22:11 +0200

    amelioration du parse

--- src/lr.c
+++ src/lr.c
@@ -1,51 +1,36 @@
 #include "lr.h"
 
-int executeLr(char * regName, char * regValue, mips micro)
+int executeLr(int index, uint value, mips * pMips)
 {
-    int i;
-    int value;
-
-    if(regValue[0] != '0' || regValue[1] != 'x')
+    switch(reg)
     {
-	WARNING_MSG("Second parameter must be a hexadecimel number (starting with 0x)");
-	return 1;
+        case -1 :
+	    pMips->regPC = value;
+	    break;
+        case -2 :
+	    pMips->regHI = value;
+	    break;
+        case -3 :
+	    pMips->regLO = value;
+	    break;
+        default :
+	    pMips->registers[index] = value;
+	    break;
     }
-    sscanf(regValue, "%x", value);
-
-    if(regName[0] != '$')
-    {
-	if(!strcmp(regName, "PC"))
-	    micro.regPC = value;
-	else if(!strcmp(regName, "HI"))
-	    micro.regHI = value;
-	else if(!strcmp(regName, "LO"))
-	    micro.regLO = value;
-	else
-	{    
-	    WARNING_MSG("First parameter must be a register name (PC, HI and LO or begin with $)\n");
-	    return 2;
-	}
-	return 0;
-    }
-
-    i = parseReg(regName);
-
-    if(i == -1)
-    {
-	WARNING_MSG("This register does not exist.\n");
-	return 3;
-    }
-    micro.registers[i] = value;
 
     return 0;
 }
 
-int parseLr(char * paramStr, mips micro)
+int parseLr(char * paramStr, mips * pMips)
 {
     char * paramStrCpy = malloc(strlen(paramStr)*sizeof(char));
     char * regName = NULL;
     char * regValue = NULL;
     char delim[] = " ";
+
+    int index = 32, i;
+    uint value;
+    char regsNames[32][] = {"$zero", "$at", "$vo", "$v1", "$a0", "$a1", "$a2", "$a3", "$t0", "$t1", "$t2", "$t3", "$t4", "$t5", "$t6", "$t7", "$s0", "$s1", "$s2", "$s3", "$s4", "$s5", "$s6", "$s7", "$t8", "$t9", "$k0", "$k1", "$gp", "$sp", "$fp", "$ra"}
 
     strcpy(paramStrCpy, paramStr); // copy of param allowing modifications
 
@@ -65,11 +50,40 @@ int parseLr(char * paramStr, mips micro)
 
     if(strtok(paramStrCpy, delim) != NULL)
     {
-	WARNING_MSG("too much parameters, lr only takes two arguments");
+	WARNING_MSG("Too much parameters, lr only takes two arguments\n");
 	return 3;
     }
 
-    executeLr(regName, regValue, micro); // rendition of parameters
+    if(sscanf(regValue, "%x", &value) == EOF)
+    {
+	WARNING_MSG("Second parameter must be a hexadecimel number (starting with 0x)\n");
+	return 4;
+    }
+
+    if(regName[0] != '$')
+    {
+	if(!strcmp(regName, "PC"))
+	    index = -1:
+	else if(!strcmp(regName, "HI"))
+	    index = -2;
+	else if(!strcmp(regName, "LO"))
+	    index = -3;
+    }
+
+    sscanf(regName + 1, "%d", &index);
+    for(i = 0; i < 32; i++)
+    {
+	if(!strcmp(regName, regsNames[i]))
+	    index = i;
+    }
+
+    if(index > 31)
+    {
+	WARNING_MSG("This register does not exist\n");
+	return 5;
+    }
+
+    executeLr(index, value, pMips); // change register value
 
     return 0;
 }
