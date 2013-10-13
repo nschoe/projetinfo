c7bd778a5a113cf1acc1c1155e74435b37dabe36
nschoe <ns.schoe@gmail.com>
2013-10-13 18:58:34 +0200

    Implemented 'dm' command : recognizes the formats addr, addr:nbBytes and addr1~addr2

--- src/dm.c
+++ src/dm.c
@@ -7,6 +7,34 @@ int executeDm()
 
 int parseDm( char * paramStr )
 {
-    printf( "given : '%s'\n", paramStr );
+    uint v1 = 0;
+    uint v2 = 0;
+
+    /* First check the format of the param : 
+       addr
+       addr:nbBytes
+       addr1~addr2
+    */
+    // First check for addr:nbBytes
+    if( 2 == sscanf( paramStr, "%x:%d", &v1, &v2 ) )
+    {
+	printf( "Format is addr:nyBytes : %#x:%d\n", v1, v2 );
+    }
+    // Then check for addr1~addr2
+    else if( 2 == sscanf( paramStr, "%x~%x", &v1, &v2 ) )
+    {
+	printf( "Format is addr1~addr2 : %#x~%#x\n", v1, v2 );
+    }
+    // Then only left is addr
+    else if( 1 == sscanf( paramStr, "%x", &v1 ) )
+    {
+	printf( "Format is addr : %#x\n", v1 );
+    }
+    // Not a good format
+    else
+    {
+	WARNING_MSG( "Usage : dm <addr> | <addr>:<nyBytes> | <addr1>~<addr2> where addr, addr1 and addr2 are given in hexadecimal form and nyBytes in decimal form.\n" );
+    }
+    
     return 0;
 }
